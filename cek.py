309a4996ebfa7b45843a181a51768ba1e608bf74
yudhiiatmadja <yudhiiatmadja@webmail.umm.ac.id>
2024-08-04 14:32:38 +0700

    myporto

---
+++ cek.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+print(pd.__version__)
+
+import matplotlib
+print(matplotlib.__version__)
+
+import numpy as np
+print(np.__version__)
+
+import sklearn
+print(sklearn.__version__)
